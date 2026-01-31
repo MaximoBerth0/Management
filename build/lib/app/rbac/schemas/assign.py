8ad4683584184de5d3052f2fd25322938fe7e025
MaximoBerth0 <maximo1.berth@gmail.com>
2026-01-30 21:12:47 -0300

    chore: remove build artifacts and env file from repo

--- build/lib/app/rbac/schemas/assign.py
+++
@@ -1,4 +0,0 @@
-from pydantic import BaseModel, Field
-
-class PermissionAssign(BaseModel):
-    permission_id: int = Field(..., gt=0)
--- build/lib/app/users/constants.py
+++
@@ -1,7 +0,0 @@
-from enum import Enum
-
-class UserRole(str, Enum):
-    CLIENT = "client"
-    DRIVER = "driver"
-    EMPLOYEE = "employee"
-    ADMIN = "admin"
